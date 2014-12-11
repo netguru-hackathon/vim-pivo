787a0bbaf16042a7f717bea761c4120f5536cd99
Jakub Naliwajek <naliwajek@gmail.com>
2014-12-11 22:04:50 +0100

    Spit Ruby code into smaller parts, add STATES_MAP

--- plugin/pivo.rb
+++ plugin/pivo.rb
@@ -4,6 +4,15 @@ require 'pivotal-tracker'
 
 class Pivo
   attr_reader :project
+  STATES_MAP = {
+    'unscheduled' => ' ? ',
+    'unstarted'   => ' s ',
+    'started'     => ' f ',
+    'finished'    => ' d ',
+    'delivered'   => 'a/r',
+    'accepted'    => ' + ',
+    'rejected'    => '-s-',
+  }
 
   def initialize(token, project_id)
     PivotalTracker::Client.use_ssl = true
@@ -13,7 +22,7 @@ class Pivo
 
   def print_stories(*)
     project.stories.all.each do |story|
-      puts "  [##{story.id}] - {#{story.current_state}} #{story.name} {#{story.owned_by}}"
+      puts line_for(story)
     end
   end
 
@@ -27,6 +36,22 @@ class Pivo
 
   def find(story_id)
     @project.stories.find(story_id)
+  end
+
+  def line_for(story)
+    "  #{id_for(story)} #{state_for(story)} #{story.name} #{author_for(story)}"
+  end
+
+  def id_for(story)
+    "[##{story.id}]"
+  end
+
+  def state_for(story)
+    "[#{STATES_MAP[story.current_state]}]"
+  end
+
+  def author_for(story)
+    "(#{story.owned_by})" unless story.owned_by.nil?
   end
 end
 
